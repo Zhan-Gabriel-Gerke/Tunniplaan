614aad6ba56f40d4aacfeb665487007152bbc862
opilane <opilane@HP-CZC6297B92>
2022-01-18 10:24:26 +0200

    ,

--- Tunniplaan.py
+++ Tunniplaan.py
@@ -1,20 +1,11 @@
 from tkinter import *
 root=Tk()
-
-lable_(text="Имя").grid(row=0,column=0)
-table_name = Entry(width=30)
-table_name.grid(row=0, column=1, columnspan=3)
-
-Lable (text="столбцов").grid(row=1,column=0)
-table_column = spinbox(width=7, from_1, to=50)
-table_column.grid(row=1, column=1)
-
-Lable (text="строк").grid(row=1,column=2)
-table_column = spinbox(width=7, from_1, to=100)
-table_column.grid(row=1, column=3)
- 
-button(text="справка").grid(row=2, column=0)
-button(text="вставить").grid(row=2, column=2)
-button(text="отменить").grid(row=2, column=3)
-
+Button(text="   ").grid(row=0,column=2)
+Button(text="Понедельник:").grid(row=1,column=1)
+Button(text="Вторник:").grid(row=2,column=1)
+Button(text="Среда:").grid(row=3,column=1)
+Button(text="Четверг:").grid(row=4,column=1)
+Button(text="Пятница:").grid(row=5,column=1)
+Button(text="Суббота:").grid(row=6,column=1)
+Button(text="Воскресенье:").grid(row=7,column=1)
 root.mainloop()
